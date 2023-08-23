d37b3953da649d9e95d7dc0aebc696221bc4caaa
Pavel Barchenko <wyerer123@gmail.com>
2023-08-23 20:36:27 +0200

    create docker compose for 2 dbs

--- src/test/resources/initTest.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO TestEntity (name) VALUES ('test1');
-INSERT INTO TestEntity (name) VALUES ('test2');
-
-INSERT INTO TestEmployee (name) VALUES ('John Doe');
-INSERT INTO TestEmployee (name) VALUES ('Jane Smith');
-
-INSERT INTO TestDependent (name, status, testEmployee_id) VALUES ('Dependent 1', 'Active', 1);
-INSERT INTO TestDependent (name, status, testEmployee_id) VALUES ('Dependent 2', 'Inactive', 1);
-INSERT INTO TestDependent (name, status, testEmployee_id) VALUES ('Dependent 3', 'Active', 2);
