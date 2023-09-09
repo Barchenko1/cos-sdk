de5d04065d759c2a9c3f7463923c62299ee18741
Pavel Barchenko <PAVEL_BARCHENKO@homedepot.com>
2023-09-09 15:33:38 +0200

    upd transactionManager

--- src/main/java/com/cos/core/transaction/ITransactionManager.java
+++ src/main/java/com/cos/core/transaction/ITransactionManager.java
@@ -1,8 +1,5 @@
 package com.cos.core.transaction;
 
-import java.util.List;
-
 public interface ITransactionManager {
     <E> void useTransaction(E values);
-    void useTransaction(List<?> values);
 }
